35e540bf725209f0cc854393f67dfd40a5464361
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2024-03-07 16:50:11 +0700

    update my app

--- lib/presentation/my_app.dart
+++ lib/presentation/my_app.dart
@@ -47,8 +47,8 @@ class MyApp extends StatelessWidget {
             // Built-in localization of basic text for Cupertino widgets
             GlobalCupertinoLocalizations.delegate,
           ],
-          // home: _userStore.isLoggedIn ? HomeScreen() : LoginScreen(),
-          home: ProfileScreen(),
+          home: _userStore.isLoggedIn ? HomeScreen() : LoginScreen(),
+          // home: ProfileScreen(),
         );
       },
     );
