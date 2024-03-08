2a397bf09cf79771ff68b9a784a8e4625cc602dc
Tuanquan127 <ltquan20@clc.fitus.edu.vn>
2024-03-08 12:04:32 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,19 +6,16 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:boilerplate/di/service_locator.dart';
-import 'package:boilerplate/firebase_options.dart';
 import 'package:boilerplate/main.dart';
 import 'package:boilerplate/presentation/my_app.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    WidgetsFlutterBinding.ensureInitialized();
     await setPreferredOrientations();
     await ServiceLocator.configureDependencies();
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
   });
