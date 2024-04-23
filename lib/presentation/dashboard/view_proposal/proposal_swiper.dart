a31ddf2419d144bee60dd3fcbc013b885aabb19c
Tuanquan127 <tuanquan127@gmail.com>
2024-04-23 23:15:17 +0700

    Edit card item and change swipe logic

--- lib/presentation/dashboard/view_proposal/proposal_swiper.dart
+++ lib/presentation/dashboard/view_proposal/proposal_swiper.dart
@@ -5,6 +5,7 @@ import 'package:boilerplate/core/widgets/main_app_bar_widget.dart';
 import 'package:boilerplate/di/service_locator.dart';
 import 'package:boilerplate/domain/entity/project/project_entities.dart';
 import 'package:boilerplate/domain/entity/project/proposal_list.dart';
+import 'package:boilerplate/presentation/dashboard/chat/flutter_chat_types.dart';
 import 'package:boilerplate/presentation/dashboard/components/proposal_card_item.dart';
 import 'package:boilerplate/presentation/dashboard/components/swiper_button.dart';
 import 'package:boilerplate/presentation/dashboard/store/project_store.dart';
@@ -38,17 +39,39 @@ class _ProposalSwiperState extends State<ProposalSwiper>
   late Future<ProposalList> future;
   static const double maxAngle = 12;
   double defaultOffSetX = 13.1;
+  late Proposal current;
+  int index = 0;
 
   @override
   void initState() {
     if (widget.project.proposal != null &&
         widget.project.proposal!.isNotEmpty) {
-      future = _projectStore.getProjectProposals(widget.project);
+      future = _projectStore.getProjectProposals(widget.project,
+          filter: (element) => element.hiredStatus == HireStatus.notHired);
     } else {
-      future = Future.value(ProposalList(proposals: widget.project.proposal));
+      future = Future.value(ProposalList(
+          proposals: widget.project.proposal!
+              .where((element) => element.hiredStatus == HireStatus.notHired)
+              .toList()));
     }
 
+    // _shakeCard();
     super.initState();
+  }
+
+  onLeftButtonPress() {
+    if (current.hiredStatus != HireStatus.pending) {
+      changeStatus(HireStatus.pending);
+    }
+    // ToDo
+    Navigator.push(
+        context,
+        MaterialPageRoute2(
+          routeName: Routes.message,
+          arguments: ChatUser(
+              id: current.student.objectId!,
+              firstName: current.student.fullName),
+        ));
   }
 
   @override
@@ -57,6 +80,10 @@ class _ProposalSwiperState extends State<ProposalSwiper>
       appBar: _buildAppBar(),
       body: _buildBody(),
     );
+  }
+
+  Future changeStatus(HireStatus status) async {
+    _projectStore.changeToStatus(status, current);
   }
 
   Widget _buildBody() {
@@ -86,6 +113,8 @@ class _ProposalSwiperState extends State<ProposalSwiper>
                     child: const Text('There is no cards to display'),
                   );
                 } else {
+                  current = snapshot.data!.proposals![0];
+
                   return Stack(children: [
                     Column(
                       children: [
@@ -116,7 +145,7 @@ class _ProposalSwiperState extends State<ProposalSwiper>
                                 _cardStateStore.changeOpacity(
                                     (position.angle.abs().roundToDouble() /
                                             maxAngle)
-                                        .clamp(0, 1));
+                                        .clamp(0.5, 1));
 
                                 if (position.offset.toAxisDirection() ==
                                     AxisDirection.left) {
@@ -161,44 +190,47 @@ class _ProposalSwiperState extends State<ProposalSwiper>
                                         SizedBox(
                                           height: 470,
                                           child: Observer(
-                                            builder: (context) =>
-                                                AnimatedOpacity(
-                                              opacity: _cardStateStore.opacity,
-                                              duration: const Duration(
-                                                  milliseconds: 200),
-                                              child: Card(
-                                                elevation: 8,
-                                                shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            32)),
-                                                color: _cardStateStore.index ==
-                                                        index
-                                                    ? _cardStateStore.color
-                                                    : null,
-                                                child: Container(
-                                                  padding: const EdgeInsets
-                                                      .symmetric(
-                                                      horizontal: 10),
-                                                  alignment: _cardStateStore
-                                                              .actionName ==
-                                                          HireStatus
-                                                              .notHired.title
-                                                      ? Alignment.topRight
-                                                      : Alignment.topLeft,
-                                                  child: _cardStateStore
+                                            builder: (context) => IgnorePointer(
+                                              child: AnimatedOpacity(
+                                                opacity:
+                                                    _cardStateStore.opacity,
+                                                duration: const Duration(
+                                                    milliseconds: 200),
+                                                child: Card(
+                                                  elevation: 8,
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              32)),
+                                                  color: _cardStateStore
                                                               .index ==
                                                           index
-                                                      ? (Text(
-                                                          _cardStateStore
-                                                              .actionName,
-                                                          style:
-                                                              const TextStyle(
-                                                                  color: Colors
-                                                                      .white,
-                                                                  fontSize:
-                                                                      30)))
-                                                      : Container(),
+                                                      ? _cardStateStore.color
+                                                      : null,
+                                                  child: Container(
+                                                    padding: const EdgeInsets
+                                                        .symmetric(
+                                                        horizontal: 10),
+                                                    alignment: _cardStateStore
+                                                                .actionName ==
+                                                            HireStatus
+                                                                .notHired.title
+                                                        ? Alignment.topRight
+                                                        : Alignment.topLeft,
+                                                    child: _cardStateStore
+                                                                .index ==
+                                                            index
+                                                        ? (Text(
+                                                            _cardStateStore
+                                                                .actionName,
+                                                            style:
+                                                                const TextStyle(
+                                                                    color: Colors
+                                                                        .white,
+                                                                    fontSize:
+                                                                        30)))
+                                                        : Container(),
+                                                  ),
                                                 ),
                                               ),
                                             ),
@@ -221,11 +253,14 @@ class _ProposalSwiperState extends State<ProposalSwiper>
                               const SizedBox(
                                 width: 20,
                               ),
-                              customSwipeLeftButton(controller, () {}),
+                              customSwipeLeftButton(controller,
+                                  onLeftButtonPress, current.hiredStatus),
                               const SizedBox(
                                 width: 10,
                               ),
-                              customSwipeRightButton(controller, () {}),
+                              customSwipeRightButton(controller, () {
+                                changeStatus(HireStatus.offer);
+                              }),
                               const SizedBox(
                                 width: 20,
                               ),
@@ -253,21 +288,21 @@ class _ProposalSwiperState extends State<ProposalSwiper>
         log('The card was swiped to the : ${activity.direction}');
         _cardStateStore.reset();
         _cardStateStore.index = targetIndex;
+        current = _projectStore.currentProps.proposals![targetIndex];
         // print(_cardStateStore.index);
+        if (_projectStore.currentProps.proposals == null ||
+            _projectStore.currentProps.proposals!.isEmpty) return;
 
         if (activity.direction == AxisDirection.right) {
           // ToDo: update proposal status value = 2
-          if (_projectStore.currentProps.proposals == null ||
-              _projectStore.currentProps.proposals!.isEmpty) return;
           _projectStore.currentProps.proposals![targetIndex].hiredStatus =
               HireStatus.offer;
           _projectStore.updateProposal(
               _projectStore.currentProps.proposals![targetIndex],
               _userStore.user!.studentProfile!.objectId!);
         } else {
-          if (_projectStore.currentProps.proposals == null ||
-              _projectStore.currentProps.proposals!.isEmpty) return;
           // Reject proposal
+          _projectStore.currentProps.proposals![targetIndex].enabled = false;
           _projectStore.currentProps.proposals![targetIndex].hiredStatus =
               HireStatus.notHired;
           _projectStore.updateProposal(
@@ -299,7 +334,7 @@ class _ProposalSwiperState extends State<ProposalSwiper>
     Navigator.pop(context);
   }
 
-  // Animates the card back and forth to teach the user that it is swipable.
+  // Animates the card back and forth to teach the user that it is swipeable.
   // ignore: unused_element
   Future<void> _shakeCard() async {
     const double distance = 30;
